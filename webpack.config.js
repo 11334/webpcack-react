9e047b0fbdeef12d14a67bde133021f8f3392446
JennC <2843274115LL@gmail.com>
2023-09-25 23:21:40 +0800

    clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 
 const path = require('path')
@@ -78,6 +79,7 @@ module.exports = {
             filename: '[name]-[hash].css',
             chunkFilename: '[id].css',
         }),
+        new CleanWebpackPlugin()
 
     ],
 
