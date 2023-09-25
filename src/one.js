acdf6fc0e99a635de99c602345f6f6ed29b60234
JennC <2843274115LL@gmail.com>
2023-09-25 20:19:13 +0800

    装饰器语法

--- src/one.js
+++ src/one.js
@@ -3,3 +3,9 @@ const test = (num) => {
     console.log("测试箭头函数 @babel/preset-env" + num);
 }
 test(Two.y);
+function testable(target) {
+    target.isTestable = true;
+}
+@testable
+class MyTestableClass { }
+console.log("装饰器语法", MyTestableClass.isTestable) // true
