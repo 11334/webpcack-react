e68a18e85ff3297866e04f904a9d8c26f01a047e
JennC <2843274115LL@gmail.com>
2023-09-25 21:29:48 +0800

    css-loader和style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,12 @@ module.exports = {
                     //     presets: ['@babel/preset-env', '@babel/preset-react']
                     // }
                 }
-            }
+            },
+            {
+                test: /\.css$/,//要监控的文件
+                use: ['style-loader', 'css-loader'],//横着写的先调后面的再调前面的，先把css通过import引入然后再去找多个import把多个css文件合成一个再交给style-loader,style-loader把他们转换成js然后打包进去
+            },
+
         ]
     },
     plugins: [
@@ -35,17 +40,17 @@ module.exports = {
         new HtmlWebpackPlugin({ //假设是前台应用入口
             title: '首页',
             filename: "index.html",
-            template: "./public/index.html",
+            template: "./public/index.html",//注入打包生成的 js/css 文件
             chunks: ["index"]    //chunks指定需要引入的入口模块的键名 index:"./src/index.js"
         }),
         new HtmlWebpackPlugin({//假设是后台应用入口one:"./src/one.js"
             title: 'One',
             filename: "one.html",
-            template: "./public/one.html",
+            template: "./public/one.html",//注入打包生成的 js/css 文件
             chunks: ["one"] //chunks指定需要引入的入口模块的键名 one:"./src/one.js"
         })
     ],
-        
+
     devtool: "source-map",
 
 }
